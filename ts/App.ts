b36de435a4598f04001f3b4124ff63dc18378be0
Lionel Pham <phamkyanh8668@gmail.com>
2020-04-02 00:49:32 +0700

    Get Id Product click

--- ts/App.ts
+++ ts/App.ts
@@ -2,3 +2,11 @@ import {ManageProduct} from 'ts/ManageProduct';
 var productrelated = new ManageProduct();
 var relatedproducts = document.getElementById('relatedproducts');
 relatedproducts.innerHTML = productrelated.getProduct();
+var buynows = document.querySelectorAll('.buynow');
+var length = buynows.length;
+for (let i:number = 0; i < length; ++i) {
+	buynows[i].addEventListener("click", function(){
+		let id:number = this.getAttribute('data-idsp');
+		console.log(id);
+	}
+}
